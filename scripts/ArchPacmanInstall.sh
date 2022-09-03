e33f93ecd72003b129c6e99f44b21b5993483dcf
Sonder Merak <merak19981999@gmail.com>
2022-09-03 10:11:20 +0800

    modify hot keys: some super -> alt

--- scripts/ArchPacmanInstall.sh
+++ scripts/ArchPacmanInstall.sh
@@ -12,11 +12,14 @@ pacman -S --noconfirm xorg-server xorg xorg-xinit xorg-xsetroot
 # Install fonts
 pacman -S --noconfirm nerd-fonts ttf-dejavu wqy-zenhei noto-fonts-cjk noto-fonts-emoji noto-fonts-extra adobe-source-code-pro-fonts
 # Install arch-bspwm
-pacman -S --noconfirm bspwm sxhkd polybar alacritty rofi feh picom neofetch sddm
+pacman -S --noconfirm bspwm sxhkd polybar alacritty rofi feh picom neofetch yay
 # Install language
 pacman -S --noconfirm go cargo nodejs yarn lua
 # Install software
-pacman -S --noconfirm chromium firefox exa eva bat git-delta ripgrep fd wget unzip rsync
+pacman -S --noconfirm chromium firefox exa eva bat git-delta fd wget unzip rsync
+yay -S --noconfirm just-git starship-git ripgrep-git tealdeer-git navi-git sac-git choose-git
+# Install fnm
+curl -fsSL https://fnm.vercel.app/install | bash
 # Install vm tools
 ## for orcale
 pacman -S --noconfirm virtualbox virtualbox-host-dkms virtualbox-guest-iso virtualbox-guest-utils
@@ -24,3 +27,13 @@ pacman -S --noconfirm virtualbox virtualbox-host-dkms virtualbox-guest-iso virtu
 pacman -S --noconfirm open-vm-tools gtkmm3
 # xian ka qu dong wei ma
 pacman -S --noconfirm intel-ucode amd-ucode mesa vulkan-intel xf86-video-intel
+# fctix
+pacman -S --noconfirm fcitx5-im fcitx5-chinese-addons fcitx5-pinyin-zhwiki fitx5-material-color
+yay -S fcitx5-pinyin-moegirl fcitx5-material-color
+# Install sddm
+pacman -S --noconfirm sddm gst-libav phonon-qt5-gstreamer gst-plugins-good qt5-quickcontrols qt5-graphicaleffects qt5-multimedia
+
+yay -S --noconfirm sddm-theme-aerial-git
+mv aerial-sddm-theme /usr/share/sddm/themes/
+
+yay -Yc

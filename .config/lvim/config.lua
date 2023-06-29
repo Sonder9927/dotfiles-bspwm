42885ad4692fa7ffac4a9b773d439f37b2a68c88
Sonder Merak <merak19981999@gmail.com>
2023-06-29 14:20:02 +0800

    lvim ide

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -1,6 +1,33 @@
+-- using fish may be slow for lvim
+-- vim.opt.shell = "/bin/sh"
+
+-- rust keymap
+
+lvim.builtin.which_key.mappings["r"] = {
+	name = "Rust",
+	r = { "<cmd>RustRunnables<Cr>", "Runnables" },
+	t = { "<cmd>lua _CARGO_TEST()<cr>", "Cargo Test" },
+	m = { "<cmd>RustExpandMacro<Cr>", "Expand Macro" },
+	c = { "<cmd>RustOpenCargo<Cr>", "Open Cargo" },
+	p = { "<cmd>RustParentModule<Cr>", "Parent Module" },
+	d = { "<cmd>RustDebuggables<Cr>", "Debuggables" },
+	v = { "<cmd>RustViewCrateGraph<Cr>", "View Crate Graph" },
+	R = {
+		"<cmd>lua require('rust-tools/workspace_refresh')._reload_workspace_from_cargo_toml()<Cr>",
+		"Reload Workspace",
+	},
+	o = { "<cmd>RustOpenExternalDocs<Cr>", "Open External Docs" },
+	y = { "<cmd>lua require'crates'.open_repository()<cr>", "[crates] open repository" },
+	P = { "<cmd>lua require'crates'.show_popup()<cr>", "[crates] show popup" },
+	i = { "<cmd>lua require'crates'.show_crate_popup()<cr>", "[crates] show info" },
+	f = { "<cmd>lua require'crates'.show_features_popup()<cr>", "[crates] show features" },
+	D = { "<cmd>lua require'crates'.show_dependencies_popup()<cr>", "[crates] show dependencies" },
+}
+
 -- keymappings <https://www.lunarvim.org/docs/configuration/keybindings>
 -- add your own keymapping
--- -- Use which-key to add extra bindings with the leader-key prefix
+-- Use which-key to add extra bindings with the leader-key prefix
+-- Sourcery keymap
 lvim.builtin.which_key.mappings["a"] = {
 	name = "Sourcery",
 	a = { "<plug>(coc-codeaction-cursor)", "codeaction-cursor" },
@@ -10,13 +37,61 @@ lvim.builtin.which_key.mappings["a"] = {
 	p = { "<plug>(coc-diagnostic-prev)", "prev" },
 	n = { "<plug>(coc-diagnostic-next)", "next" },
 }
+-- swap conda env keymap
+lvim.builtin.which_key.mappings["lc"] = {
+	"<cmd>lua require('swenv.api').pick_venv()<cr>",
+	"Choose Env",
+}
+-- run python code
+lvim.builtin.which_key.mappings["lp"] = {
+	":w<cr>:sp |terminal python %<cr>",
+	"Run Python",
+}
+-- Trouble
+lvim.builtin.which_key.mappings["t"] = {
+	name = "Trouble",
+	r = { "<cmd>Trouble lsp_references<cr>", "References" },
+	d = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+	t = { "<cmd>Trouble lsp_type_definitions<cr>", "Definitions" },
+	w = { "<cmd>Trouble workspace_diagnostics<cr>", "Diagnostics" },
+	D = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+	f = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+	l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+}
+
+-- Additional binding for debug
+lvim.builtin.which_key.mappings["dm"] = { "<cmd>lua require('neotest').run.run()<cr>", "Test Method" }
+lvim.builtin.which_key.mappings["dM"] =
+	{ "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Test Method DAP" }
+lvim.builtin.which_key.mappings["df"] = {
+	"<cmd>lua require('neotest').run.run({vim.fn.expand('%')})<cr>",
+	"Test Class",
+}
+lvim.builtin.which_key.mappings["dF"] = {
+	"<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>",
+	"Test Class DAP",
+}
+lvim.builtin.which_key.mappings["dS"] = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Test Summary" }
+
+-- automatically install python syntax highlighting
+-- lvim.builtin.treesitter.highlight.enable = true
+-- lvim.builtin.treesitter.ensure_installed = {
+-- 	"python",
+-- 	"lua",
+-- 	"rust",
+-- 	"toml",
+-- }
 
 -- linters and formatters <https://www.lunarvim.org/docs/languages#lintingformatting>
+-- setup formatting
+vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "rust_analyzer" })
 local formatters = require("lvim.lsp.null-ls.formatters")
 formatters.setup({
 	{ command = "black" },
-	{ command = "stylua" },
+	{ command = "stylua", filetypes = { "lua" } },
 	{ command = "deno_fmt" },
+	{ command = "taplo", filetypes = { "toml" } },
+	{ command = "rustfmt", filetypes = { "rust" } },
 	{ command = "beautysh" },
 	{ command = "fprettify", filetypes = { "fortran" }, args = { "--silent" } },
 	-- {
@@ -26,9 +101,10 @@ formatters.setup({
 	-- },
 })
 
+-- setup linting
 local linters = require("lvim.lsp.null-ls.linters")
 linters.setup({
-	{ command = "flake8", filetypes = { "python" } },
+	{ command = "flake8", filetypes = { "python" }, args = { "--ignore=E203" } },
 	-- {
 	-- 	command = "shellcheck",
 	-- 	args = { "--severity", "warning" },
@@ -36,14 +112,160 @@ linters.setup({
 })
 
 -- Additional Plugins <https://www.lunarvim.org/docs/plugins#user-plugins>
+-- install plugins
 lvim.plugins = {
+	-- trouble
+	{
+		"folke/trouble.nvim",
+		cmd = "TroubleToggle",
+	},
+	-- python
 	{
 		"neoclide/coc.nvim",
 		branch = "master",
 		build = "yarn install --frozen-lockfile",
 	},
-	--     {
-	--       "folke/trouble.nvim",
-	--       cmd = "TroubleToggle",
-	--     },
+	"AckslD/swenv.nvim",
+	"stevearc/dressing.nvim",
+	"mfussenegger/nvim-dap-python",
+	"nvim-neotest/neotest",
+	"nvim-neotest/neotest-python",
+	-- rust
+	"simrat39/rust-tools.nvim",
+	{
+		"saecki/crates.nvim",
+		version = "v0.3.0",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("crates").setup({
+				null_ls = {
+					enabled = true,
+					name = "crates.nvim",
+				},
+				popup = {
+					border = "rounded",
+				},
+			})
+		end,
+	},
+	{
+		"j-hui/fidget.nvim",
+		config = function()
+			require("fidget").setup()
+		end,
+	},
 }
+
+-- setup debug adapter
+lvim.builtin.dap.active = true
+local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
+local codelldb_path = mason_path .. "bin/codelldb"
+local liblldb_path = mason_path .. "packages/codelldb/extension/lldb/lib/liblldb"
+-- local this_os = vim.loop.os_uname().sysname
+
+-- -- The path in windows is different
+-- if this_os:find "Windows" then
+--   codelldb_path = mason_path .. "packages\\codelldb\\extension\\adapter\\codelldb.exe"
+--   liblldb_path = mason_path .. "packages\\codelldb\\extension\\lldb\\bin\\liblldb.dll"
+-- else
+--   -- The liblldb extension is .so for linux and .dylib for macOS
+--   liblldb_path = liblldb_path .. (this_os == "Linux" and ".so" or ".dylib")
+-- end
+pcall(function()
+	require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
+	require("rust-tools").setup({
+		tools = {
+			executor = require("rust-tools/executors").termopen, -- can be quickfix or termopen
+			reload_workspace_from_cargo_toml = true,
+			runnables = {
+				use_telescope = true,
+			},
+			inlay_hints = {
+				auto = true,
+				only_current_line = false,
+				show_parameter_hints = false,
+				parameter_hints_prefix = "<-",
+				other_hints_prefix = "=>",
+				max_len_align = false,
+				max_len_align_padding = 1,
+				right_align = false,
+				right_align_padding = 7,
+				highlight = "Comment",
+			},
+			hover_actions = {
+				border = "rounded",
+			},
+			on_initialized = function()
+				vim.api.nvim_create_autocmd({ "BufWritePost", "BufEnter", "CursorHold", "InsertLeave" }, {
+					pattern = { "*.rs" },
+					callback = function()
+						local _, _ = pcall(vim.lsp.codelens.refresh)
+					end,
+				})
+			end,
+		},
+		dap = {
+			-- adapter= codelldb_adapter,
+			adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
+		},
+		server = {
+			on_attach = function(client, bufnr)
+				require("lvim.lsp").common_on_attach(client, bufnr)
+				local rt = require("rust-tools")
+				vim.keymap.set("n", "K", rt.hover_actions.hover_actions, { buffer = bufnr })
+			end,
+
+			capabilities = require("lvim.lsp").common_capabilities(),
+			settings = {
+				["rust-analyzer"] = {
+					lens = {
+						enable = true,
+					},
+					checkOnSave = {
+						enable = true,
+						command = "clippy",
+					},
+				},
+			},
+		},
+	})
+end)
+
+lvim.builtin.dap.on_config_done = function(dap)
+	dap.adapters.codelldb = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path)
+	dap.configurations.rust = {
+		{
+			name = "Launch file",
+			type = "codelldb",
+			request = "launch",
+			program = function()
+				return vim.fn.input({ "Path to executable: ", vim.fn.getcwd() .. "/", "file" })
+			end,
+			cwd = "${workspaceFolder}",
+			stopOnEntry = false,
+		},
+	}
+end
+
+-- setup switching
+require("swenv").setup({
+	post_set_venv = function()
+		vim.cmd("LspRestart")
+	end,
+})
+
+-- setup testing
+require("neotest").setup({
+	adapters = {
+		require("neotest-python")({
+			-- Extra arguments for nvim-dap configuration
+			-- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+			dap = {
+				justMyCode = false,
+				console = "integratedTerminal",
+			},
+			args = { "--log-level", "DEBUG", "--quiet" },
+			runner = "pytest",
+		}),
+	},
+})
--- .dotfiles-bspwm/README.md
+++ .dotfiles-bspwm/README.md
@@ -3,6 +3,7 @@
 ---
 
 ## Info
+
 [Look to my dotfiles](https://github.com/Sonder9927/.dotfiles).
 
 This version is for my new ArchLinux which use bspwm+sxhkd.
@@ -10,31 +11,59 @@ This version is for my new ArchLinux which use bspwm+sxhkd.
 And this time I add new content about polybar.
 
 ## Addition
+
 ### Lvim
 
-Use `black` as formatter and `flake8` as linter
-and write python scripts with the extension `coc-pyright`.
+- language ide:
 
-If you hope don't install `pyright` automaticly,
-change file `~/.local/share/lunarvim/lvim/lua/lvim/lsp/config.lua`:
+see [starter.lvim](https://github.com/LunarVim/starter.lvim)
+
+- useful keymaps:
+
+- vim cmd
+  - `<C-i>` to go forward
+  - `<C-o>` and `''` to go back
+  - `:sp` split panel
+  - `<C-j/k>` jump panel
+
+- leader cmd
+  - python
+    - `as` sourcery show
+    - `af` sourcery fix
+    - `lc` switch env
+
+  - rust
+    - `rr` cargo runnables
+    - `rc` open `Cargo.toml`
+    - `ry` open creates repository
+
+- self cmd
+  - `lp` run python code
+
+- details:
+
+Use `black` as formatter and `flake8` as linter and write python scripts with
+the extension `coc-pyright`.
+
+If you hope don't install `pyright` automaticly, change
+`lvim.installer.setup({automatic_installation={exclude={"pyright"}}})`, which in
+file `~/.local/share/lunarvim/lvim/lua/lvim/lsp/config.lua`:
 
 ```lua
-
   ...
 
   installer = {
     setup = {
       ensure_installed = {},
       automatic_installation = {
-        exclude = { "pyright" },
+        exclude = {},
       },
     },
   },
 
   ...
-
 ```
 
 ## Wishes
-Write the scripts to install Arch and set my configuration.
 
+Write the scripts to install Arch and set my configuration.

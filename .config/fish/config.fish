0b9496a9d1f9d91dbd265f211f0839af6a9956e4
Sonder Merak <merak19981999@gmail.com>
2023-07-12 10:58:31 +0800

    ghci for haskell

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -16,3 +16,5 @@ eval /home/merak/Applications/miniconda3/bin/conda "shell.fish" "hook" $argv | s
 # <<< conda initialize <<<
 
 thefuck --alias | source
+
+set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin /home/merak/.ghcup/bin $PATH # ghcup-env
